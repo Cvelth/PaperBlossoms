5a72d07e43974bedc49c9bbd12b8e28f82d3ccc2
cvelth <jcvelth@gmail.com>
2025-11-22 16:07:34 +0200

    add `Legacies of War` book to the database

--- PaperBlossoms/data/scripts/add_enums.py
+++ PaperBlossoms/data/scripts/add_enums.py
@@ -175,7 +175,7 @@ def get_advantages(data_dir):
 
 
 def get_books():
-    books_enum = ['Core', 'EE', 'SL', 'Mantis', 'GMK', 'CoS', 'PoW', 'FoV', 'CR', 'DT', 'WotW', 'CotFW']
+    books_enum = ['Core', 'EE', 'SL', 'Mantis', 'GMK', 'CoS', 'PoW', 'FoV', 'CR', 'DT', 'WotW', 'CotFW', 'Legacies of War']
 
     return books_enum
 
